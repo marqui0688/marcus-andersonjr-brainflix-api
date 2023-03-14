6df5a134a081d7202783d8fe7795ece1cd0f3241
Marcus Anderson <marqui0688@gmail.com>
2023-03-14 15:23:57 -0400

    Worked on routes for videos and video ID's. Also included code that generates a random ID for new videos that are uploaded.

--- routes/videos.js
+++ routes/videos.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const videoData = readVideo();
+///// GENERATE RANDOM ID ////////
+const randomId = cypto.randomUUID();
+const newVideo = { ...req.body, id: randomId };
+videoData.push(newVideo);
 
+/////    GET    ///////
 router.get("/", (request, response) => {
   let allData = JSON.parse(fs.readFileSync("./data/videos.json"));
   response.send(allData);
+});
+
+router.get("/:videoId", (request, response) => {
+  const videoId = req.params.videoId;
+  const videoData = json.parse(fs.readFileSync(data / videos.json));
+});
+/////    POST NEW VIDEO    ///////
+router.post("/", (request, response) => {
+  const { id, author, title, comment } = req.body;
 });
 
 ////Manipulate the data to only bring back certain properties (ID, image, title, channel). Try to make it so that when endpoint is hit, only part of the JSON file is shown (properties you want to see).
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const cors = require("cors");
 const port = 8080;
 const videoRoute = require("./routes/videos.js");
 
+app.use(express.static("public"));
+
+app.use("/videos", videoRoute);
+
 app.listen(port, () => {
   console.log("server started");
 });
-
-app.use("/videos", videoRoute);
